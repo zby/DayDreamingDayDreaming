bfe1eed9ead98e993b4bf80601b09c653f262a4a
Zbigniew Lukasiak <zzbbyy@gmail.com>
2025-09-06 10:31:33 +0200

    refactor(assets): remove assets/groups/* shims; project now imports top-level group_* modules

--- daydreaming_dagster/assets/groups/group_evaluation.py
+++
@@ -1,9 +0,0 @@
-"""
-Group: evaluation
-
-Re-exports for discoverability; asset code lives in:
-- daydreaming_dagster/assets/group_evaluation.py
-"""
-
-from ..group_evaluation import *  # noqa: F401,F403
-
--- daydreaming_dagster/assets/groups/group_task_definitions.py
+++
@@ -1,9 +0,0 @@
-"""
-Group: task_definitions
-
-Re-exports for discoverability; asset code lives in:
-- daydreaming_dagster/assets/group_task_definitions.py
-"""
-
-from ..group_task_definitions import *  # noqa: F401,F403
-
