4f27fff6c6c5a1fc45033944e4cb438b3a1b5cce
Zbigniew Lukasiak <zzbbyy@gmail.com>
2025-09-11 18:03:46 +0200

    phase3: stop re-exporting task assets from assets package (__init__)

--- daydreaming_dagster/assets/__init__.py
+++ daydreaming_dagster/assets/__init__.py
@@ -2,7 +2,6 @@
 
 # Import all assets from their respective modules
 from .raw_data import *
-from .group_task_definitions import *
 from .group_generation_draft import *
 from .group_generation_essays import *
 from .group_evaluation import *
